LineageOS/android_hardware_qcom_display

sdm: Support Samsung FINGERPRINT_MASK DRM property
Allow Samsung devices to enable their HBM mode when a dim layer is present.

Change-Id: I200d06d1e17dbad55715d4fc8e1ce812ff0f0ec9

libdrmutils/drm_interface.h
@@ -417,6 +417,15 @@ enum struct DRMOps {
    *      uint32_t - colorspace value bit-mask
    */
   CONNECTOR_SET_COLORSPACE,
+  /*
+   * Op: Sets fingerprint mask on this connector
+   * Arg: uint32_t - Connector ID
+   *      uint32_t - fingerprint mask
+   */
+#ifdef SEC_FINGERPRINT_MASK
+  CONNECTOR_SET_FINGERPRINT_MASK,
+#endif
+
 };
 
 enum struct DRMRotation {

sde-drm/drm_atomic_req.cpp
@@ -123,6 +123,9 @@ int DRMAtomicReq::Perform(DRMOps opcode, uint32_t obj_id, ...) {
     case DRMOps::CONNECTOR_SET_QSYNC_MODE:
     case DRMOps::CONNECTOR_SET_TOPOLOGY_CONTROL:
     case DRMOps::CONNECTOR_SET_FRAME_TRIGGER:
+#ifdef SEC_FINGERPRINT_MASK
+    case DRMOps::CONNECTOR_SET_FINGERPRINT_MASK:
+#endif
     case DRMOps::CONNECTOR_SET_COLORSPACE: {
       drm_mgr_->GetConnectorMgr()->Perform(opcode, obj_id, drm_atomic_req_, args);
     } break;

sde-drm/drm_connector.cpp
@@ -968,6 +968,25 @@ void DRMConnector::Perform(DRMOps code, drmModeAtomicReq *req, va_list args) {
       }
     } break;
 
+#ifdef SEC_FINGERPRINT_MASK
+    case DRMOps::CONNECTOR_SET_FINGERPRINT_MASK: {
+      if (!prop_mgr_.IsPropertyAvailable(DRMProperty::FINGERPRINT_MASK)) {
+        DRM_LOGE("DRMConnector::%s: Connector %d : Fingerprint Property is not available",
+                   __FUNCTION__, obj_id);
+        return;
+      }
+      uint32_t fingerprint_mask = va_arg(args, uint32_t);
+      uint32_t prop_id = prop_mgr_.GetPropertyId(DRMProperty::FINGERPRINT_MASK);
+      int ret = drmModeAtomicAddProperty(req, obj_id, prop_id, fingerprint_mask);
+      if (ret < 0) {
+          DRM_LOGE("AtomicAddProperty failed obj_id 0x%x, prop_id %d mode %d ret %d",
+                   obj_id, prop_id, fingerprint_mask, ret);
+      } else {
+        DRM_LOGD("Connector %d: Setting Fingerprint mode %d", obj_id, fingerprint_mask);
+      }
+    } break;
+#endif
+
     default:
       DRM_LOGE("Invalid opcode %d to set on connector %d", code, obj_id);
       break;

sde-drm/drm_property.cpp
@@ -165,6 +165,9 @@ DRMProperty DRMPropertyManager::GetPropertyEnum(const std::string &name) const {
   if (name == "sspp_layout") { return DRMProperty::SDE_SSPP_LAYOUT; }
   if (name == "dspp_caps") { return DRMProperty::DSPP_CAPABILITIES; }
   if (name == "SDE_DSPP_RC_MASK_V1") { return DRMProperty::DSPP_RC_MASK_V1; }
+#ifdef SEC_FINGERPRINT_MASK
+  if (name == "fingerprint_mask") { return DRMProperty::FINGERPRINT_MASK; }
+#endif
 
   return DRMProperty::INVALID;
 }

sde-drm/drm_property.h
@@ -170,6 +170,9 @@ enum struct DRMProperty {
   SDE_SSPP_LAYOUT,
   DSPP_RC_MASK_V1,
   DSPP_CAPABILITIES,
+#ifdef SEC_FINGERPRINT_MASK
+  FINGERPRINT_MASK,
+#endif
 
   // Insert above
   MAX

sdm/libs/core/drm/hw_device_drm.h
@@ -305,6 +305,9 @@ class HWDeviceDRM : public HWInterface {
   bool autorefresh_ = false;
   std::unique_ptr<HWColorManagerDrm> hw_color_mgr_ = {};
   bool seamless_mode_switch_ = false;
+#ifdef SEC_FINGERPRINT_MASK
+  bool current_mask_state_ = false;
+#endif
 };
 
 }  // namespace sdm

yaap/hardware_qcom-caf_sm8350_display

libdrmutils/drm_interface.h
@@ -435,6 +435,14 @@ enum struct DRMOps {
    *      uint32_t - colorspace value bit-mask
    */
   CONNECTOR_SET_COLORSPACE,
+#ifdef CONNECTOR_PROP_UDFPS
+  /*
+   * Op: Sets fingerprint mask on this connector
+   * Arg: uint32_t - Connector ID
+   *      uint32_t - fingerprint mask
+   */
+  CONNECTOR_SET_FINGERPRINT_MASK,
+#endif
 };
 
 enum struct DRMRotation {

sde-drm/drm_atomic_req.cpp
@@ -125,6 +125,9 @@ int DRMAtomicReq::Perform(DRMOps opcode, uint32_t obj_id, ...) {
     case DRMOps::CONNECTOR_SET_QSYNC_MODE:
     case DRMOps::CONNECTOR_SET_TOPOLOGY_CONTROL:
     case DRMOps::CONNECTOR_SET_FRAME_TRIGGER:
+#ifdef CONNECTOR_PROP_UDFPS
+    case DRMOps::CONNECTOR_SET_FINGERPRINT_MASK:
+#endif
     case DRMOps::CONNECTOR_SET_COLORSPACE: {
       drm_mgr_->GetConnectorMgr()->Perform(opcode, obj_id, drm_atomic_req_, args);
     } break;

sde-drm/drm_connector.cpp
@@ -974,6 +974,25 @@ void DRMConnector::Perform(DRMOps code, drmModeAtomicReq *req, va_list args) {
       }
     } break;
 
+#ifdef CONNECTOR_PROP_UDFPS
+    case DRMOps::CONNECTOR_SET_FINGERPRINT_MASK: {
+      if (!prop_mgr_.IsPropertyAvailable(DRMProperty::FINGERPRINT_MASK)) {
+        DRM_LOGE("DRMConnector::%s: Connector %d: Fingerprint property is not available",
+                 __FUNCTION__, obj_id);
+        return;
+      }
+      uint32_t fingerprint_mask = va_arg(args, uint32_t);
+      uint32_t prop_id = prop_mgr_.GetPropertyId(DRMProperty::FINGERPRINT_MASK);
+      int ret = drmModeAtomicAddProperty(req, obj_id, prop_id, fingerprint_mask);
+      if (ret < 0) {
+        DRM_LOGE("AtomicAddProperty failed obj_id 0x%x, prop_id %d mode %d ret %d",
+                 obj_id, prop_id, fingerprint_mask, ret);
+      } else {
+        DRM_LOGD("Connector %d: Setting Fingerprint mode %d", obj_id, fingerprint_mask);
+      }
+    } break;
+#endif
+
     default:
       DRM_LOGE("Invalid opcode %d to set on connector %d", code, obj_id);
       break;

sde-drm/drm_property.cpp
@@ -168,6 +168,9 @@ DRMProperty DRMPropertyManager::GetPropertyEnum(const std::string &name) const {
   if (name == "dspp_caps") { return DRMProperty::DSPP_CAPABILITIES; }
   if (name == "SDE_SPR_INIT_CFG_V1") { return DRMProperty::SPR_INIT_CFG_V1; }
   if (name == "SDE_DSPP_RC_MASK_V1") { return DRMProperty::DSPP_RC_MASK_V1; }
+#ifdef CONNECTOR_PROP_UDFPS
+  if (name == "fingerprint_mask") { return DRMProperty::FINGERPRINT_MASK; }
+#endif
 
   return DRMProperty::INVALID;
 }

sde-drm/drm_property.h
@@ -173,6 +173,9 @@ enum struct DRMProperty {
   DSPP_CAPABILITIES,
   SPR_INIT_CFG_V1,
   DSPP_RC_MASK_V1,
+#ifdef CONNECTOR_PROP_UDFPS
+  FINGERPRINT_MASK,
+#endif
 
   // Insert above
   MAX

sdm/libs/core/drm/hw_device_drm.h
@@ -296,6 +296,9 @@ class HWDeviceDRM : public HWInterface {
   bool autorefresh_ = false;
   std::unique_ptr<HWColorManagerDrm> hw_color_mgr_ = {};
   bool seamless_mode_switch_ = false;
+#ifdef CONNECTOR_PROP_UDFPS
+  bool current_mask_state_ = false;
+#endif
 };
 
 }  // namespace sdm
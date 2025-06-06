yaap/hardware_qcom-caf_sm8650_display

sdm: Operate FINGERPRINT_MASK for oplus optical UDFPS
Even though oplus calls the connector property hbm_enable, it's
function is not just limited to operating HBM. It is also responsible
for other aspects of UDFPS operations like that of setFpPress so that
we don't have to handle any of these through IOCTL.

The purpose of this DRM connector property is for the userspace to let
the kernel display and fingerprint drivers know which layers are visible.
The same can be achieved by asserting with the layer name instead of setting
a magic constant for the layer ZOrder, thereby eliminating the need for
SurfaceFlinger hacks that we previously needed with prebuilt HWC.

| Bit index | Decimal | Layer       | Z-order      |
| --------- | ------- | ----------- | ------------ |
| 0         | 1       | **dim**     | `0x41000005` |
| 1         | 2       | **pressed** | `0x41000033` |
| 2         | 4       | **icon**    | `0x41000032` |
| 3         | 8       | **aod**     | `0x41000011` |

Change-Id: I9fcb119bbebf44b2cd13fb465c09cd9d332013ed

sdm/libs/dal/hw_device_drm.cpp
@@ -75,6 +75,13 @@
 
 #include "hw_device_drm.h"
 
+#ifdef OPLUS_FINGERPRINT_MASK
+#include <UdfpsExtension.h>
+#ifdef UDFPS_TOUCHED_LAYER_NAME
+#undef UDFPS_TOUCHED_LAYER_NAME
+#define UDFPS_TOUCHED_LAYER_NAME "SurfaceView[UdfpsControllerOverlay]"
+#endif
+#endif
 #ifdef UDFPS_ZPOS
 #include <display/drm/sde_drm.h>
 #endif
@@ -1262,7 +1269,7 @@ DisplayError HWDeviceDRM::PowerOff(bool teardown, SyncPoints *sync_points) {
 #ifdef SEC_FINGERPRINT_MASK
   if (IsPrimaryDisplay()) {
     if (current_mask_state_) {
-      current_mask_state_ = false;
+      current_mask_state_ = 0;
       drm_atomic_intf_->Perform(DRMOps::CONNECTOR_SET_FINGERPRINT_MASK,
           token_.conn_id, current_mask_state_);
       DLOGI("Display:%d Setting Fingerprint inDisplay Layer property = %d",
@@ -1482,6 +1489,24 @@ void HWDeviceDRM::SetupAtomic(Fence::ScopedRef &scoped_ref, HWLayersInfo *hw_lay
           token_.conn_id, current_mask_state_);
       DLOGI("Display:%d Setting Fingerprint inDisplay Layer property = %d",
             display_id_, current_mask_state_);
+    }
+  }
+#endif
+
+#ifdef OPLUS_FINGERPRINT_MASK
+  if (IsPrimaryDisplay()) {
+    uint32_t mask_state = 0;
+    for (uint32_t i = 0; i < hw_layer_count; i++) {
+      const std::string &name = hw_layers_info->hw_layers[i].layer_name;
+      if (name.find(UDFPS_DIM_LAYER_NAME) != std::string::npos) {
+        mask_state |= 0x1;
+      } else if (name.find(UDFPS_TOUCHED_LAYER_NAME) != std::string::npos) {
+        mask_state |= 0x2;
+      }
+    }
+    if (current_mask_state_ != mask_state) {
+      current_mask_state_ = mask_state;
+      drm_atomic_intf_->Perform(DRMOps::CONNECTOR_SET_FINGERPRINT_MASK, token_.conn_id, mask_state);
     }
   }
 #endif

sdm/libs/dal/hw_device_drm.h
@@ -393,7 +393,7 @@ class HWDeviceDRM : public HWInterface {
   std::unique_ptr<HWColorManagerDrm> hw_color_mgr_ = {};
   float aspect_ratio_threshold_ = 1.0;
 #ifdef CONNECTOR_PROP_UDFPS
-  bool current_mask_state_ = false;
+  uint32_t current_mask_state_ = 0;
 #endif
 };
 

yaap/hardware_qcom-caf_sm8350_display

sdm/libs/core/drm/hw_device_drm.cpp
@@ -104,6 +104,14 @@
 #include "hw_device_drm.h"
 #include "hw_info_interface.h"
 
+#ifdef OPLUS_FINGERPRINT_MASK
+#include <UdfpsExtension.h>
+#ifdef UDFPS_TOUCHED_LAYER_NAME
+#undef UDFPS_TOUCHED_LAYER_NAME
+#define UDFPS_TOUCHED_LAYER_NAME "SurfaceView[UdfpsControllerOverlay]"
+#endif
+#endif
+
 #define __CLASS__ "HWDeviceDRM"
 
 #ifndef DRM_FORMAT_MOD_QCOM_COMPRESSED
@@ -1081,7 +1089,7 @@ DisplayError HWDeviceDRM::PowerOff(bool teardown) {
 #ifdef SEC_FINGERPRINT_MASK
   if (IsPrimaryDisplay()) {
     if (current_mask_state_) {
-      current_mask_state_ = false;
+      current_mask_state_ = 0;
       drm_atomic_intf_->Perform(DRMOps::CONNECTOR_SET_FINGERPRINT_MASK,
           token_.conn_id, current_mask_state_);
       DLOGI("Display:%d Setting Fingerprint inDisplay Layer property = %d",
@@ -1271,6 +1279,24 @@ void HWDeviceDRM::SetupAtomic(Fence::ScopedRef &scoped_ref, HWLayers *hw_layers,
           token_.conn_id, current_mask_state_);
       DLOGI("Display:%d Setting Fingerprint inDisplay Layer property = %d",
             display_id_, current_mask_state_);
+    }
+  }
+#endif
+
+#ifdef OPLUS_FINGERPRINT_MASK
+  if (IsPrimaryDisplay()) {
+    uint8_t mask_state = 0;
+    for (uint32_t i = 0; i < hw_layer_count; i++) {
+      const std::string &name = hw_layers_info->hw_layers[i].layer_name;
+      if (name.find(UDFPS_DIM_LAYER_NAME) != std::string::npos) {
+        mask_state |= 0x1;
+      } else if (name.find(UDFPS_TOUCHED_LAYER_NAME) != std::string::npos) {
+        mask_state |= 0x2;
+      }
+    }
+    if (current_mask_state_ != mask_state) {
+      current_mask_state_ = mask_state;
+      drm_atomic_intf_->Perform(DRMOps::CONNECTOR_SET_FINGERPRINT_MASK, token_.conn_id, mask_state);
     }
   }
 #endif

sdm/libs/core/drm/hw_device_drm.h
@@ -297,7 +297,7 @@ class HWDeviceDRM : public HWInterface {
   std::unique_ptr<HWColorManagerDrm> hw_color_mgr_ = {};
   bool seamless_mode_switch_ = false;
 #ifdef CONNECTOR_PROP_UDFPS
-  bool current_mask_state_ = false;
+  uint8_t current_mask_state_ = 0;
 #endif
 };
 